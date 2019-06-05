e1c74909aa4ea4e0fe93623ad9b69c4b4de417ef
DonnyScript <donnyscript@gmail.com>
2019-06-05 15:34:06 -0400

    Add check double

--- CheckingFunction's.h
+++ CheckingFunction's.h
@@ -2,20 +2,28 @@
 #define CHECK_H
 #include <iostream>
 using namespace std;
-int checkInt(int& a)// To use set your int in main equal to this with that int as a parameter 
+int checkInt(int& input)// To use set your int in main equal to this with that int as inputa parameter 
 {
 	while (cin.fail())
 	{
 		cout << "Put valid number: ";
 		cin.clear();
 		cin.ignore(100, '\n');
-		cin >> a;
+		cin >> input;
 	}
-	return a;
+	return input;
 }
-
-
-
+double checkDouble(double input)
+{
+	while (cin.fail())
+	{
+		cout << "Put valid number: ";
+		cin.clear();
+		cin.ignore(100, '\n');
+		cin >> input;
+	}
+return input;
+}
 
 
 #endif // !CHECK_H
