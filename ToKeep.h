22c73a3c61ae221b22f6dbb5e8393f8849be4996
DonnyScript <37942065+DonnyScript@users.noreply.github.com>
2019-06-30 17:54:03 -0400

    Update

--- ToKeep.h
+++ ToKeep.h
@@ -3,7 +3,7 @@
 #include <iostream> 
 using namespace std;
 
-void print()//using the same one as class example 
+void print()//using the same one as class example, credit Eric Charnesky
 	{
 		for (int dash = 0; dash < 8; dash++)
 		{
@@ -15,7 +15,7 @@ void print()//using the same one as class example
 			for (int column = 0; column < 8; column++)
 			{
 				cout << "|";
-				cout << board[row][column];
+				//cout << board[row][column];// Make an char with board[][] to fill with pieces 
 			}
 			cout << "|" << endl;
 			for (int dash = 0; dash < 8; dash++)
