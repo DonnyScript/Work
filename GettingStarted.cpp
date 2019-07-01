518e83d02440db86358d4b8a8cea46f36426b6be
DonnyScript <37942065+DonnyScript@users.noreply.github.com>
2019-07-01 08:40:12 -0400

    First commit

---
+++ GettingStarted.cpp
@@ -0,0 +1,3 @@
+/*This is a guide for people intrested in C++, this will mostly cover absolute basics.
+For starters at the top of every C++ file you need "#include <iostream>" as seen below. */
+#include <iostream> 
