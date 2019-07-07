c2e9a5b62b013ccb173cc62d039cb24c7ded084d
DonnyScript <donnyscript@gmail.com>
2019-07-06 22:24:22 -0400

    Update

--- 2DVector.h
+++ 2DVector.h
@@ -28,3 +28,49 @@ void printVector()//Currently setup to print a 2D vector called Maze
 			cout << endl;
 		}
 	}
+
+
+	bool findPath(int x, int y)//Pathfinder that uses a 2D vector 
+	{
+		if((x < 0) || (y < 0) || (x >= Maze.size()) || (y >= Maze.at(x).size()) )
+		{
+			return false; 
+		}
+		if(Maze.at(x).at(y) == end)
+		{
+			return true;
+		}
+
+		if((Maze.at(x).at(y) != open) && (Maze.at(x).at(y) != start) )
+		{
+			return false;
+		}
+
+		Maze.at(x).at(y) = path;
+
+		if(findPath(x-1,y) == true)
+		{
+			return true;
+		}
+
+		if(findPath(x,y + 1) == true)
+		{
+			return true;
+		}
+
+		if(findPath(x +1,y) == true)
+		{
+			return true;
+		}
+
+		if(findPath(x,y - 1 ) == true)
+		{
+			return true; 
+		}
+
+		Maze.at(x).at(y) = 'X';
+
+		return false; 
+
+	}
+};
